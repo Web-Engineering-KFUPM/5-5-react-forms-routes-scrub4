8bb6a1c14b92b5a0b7cecee36c4caa667a292050
scrub4 <s202261640@kfupm.edu.sa>
2026-03-02 12:01:26 +0300

    3

--- 5-5-react-route-form/src/App.jsx
+++ 5-5-react-route-form/src/App.jsx
@@ -151,12 +151,19 @@ export default function App() {
       <nav className="navbar">
         <div className="brand">🧑‍💻 Student Portal</div>
         <div className="links">
-          {/*Nav links*/}
+          <NavLink to="/" end className="navlink">Home</NavLink>
+          <NavLink to="/about" className="navlink">About</NavLink>
+          <NavLink to="/registration" className="navlink">Registration</NavLink>
         </div>
       </nav>
 
       <main className="container">
-        {/*Routes*/}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="*" element={<h2>404 — Not Found</h2>} />
+        </Routes>
       </main>
 
       <footer className="footer">
